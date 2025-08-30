71e8266a0690f8f184b9ac7b2a758d6d934db337
vxd303 <vanxinhdep303@gmail.com>
2025-08-30 08:19:26 -0700

    Create scpaths_plain.h

---
+++ kernel/patch/include/uapi/gen/scpaths_plain.h
@@ -0,0 +1,7 @@
+// plaintext để generate; KHÔNG include file này ở runtime
+#define SU_PATH_PLAIN      "/system/bin/hoaqt"
+#define SUPERCMD_PLAIN     "/system/bin/tail"
+
+// có thể bổ sung thêm:
+// #define USER_INIT_SH_PATH_PLAIN "/dev/user_init.sh"
+// #define DEV_LOG_DIR_PLAIN       "/dev/user_init_log/"
