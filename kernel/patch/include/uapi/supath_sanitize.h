8ee6755bef3a654eac854f5b925dba461829285d
vxd303 <vanxinhdep303@gmail.com>
2025-08-30 21:40:08 -0700

    Update supath_sanitize.h

--- kernel/patch/include/uapi/supath_sanitize.h
+++ kernel/patch/include/uapi/supath_sanitize.h
@@ -1,12 +1,23 @@
 #pragma once
-#include "patch/include/uapi/scdefs.h"   // đã trả SUPERCMD/SU_PATH bằng getter
 
-/* Xoá mọi define cũ (nếu có) và ép về getter runtime */
+/* ĐỪNG include scdefs.h ở đây để tránh kéo theo uid_t/struct...
+   Chỉ cần decode getters là đủ. */
+#ifndef __ASSEMBLER__  /* để .S không dính header này */
+#include "patch/include/uapi/gen/scpaths_decode.h"  // kp_get_supercmd(), kp_get_su_path()
+
+/* Xoá mọi định nghĩa cũ nếu có, ép về getter runtime */
 #ifdef SUPERCMD
 # undef SUPERCMD
 #endif
 #ifdef SU_PATH
 # undef SU_PATH
 #endif
+
+/* Dùng getters – không tạo literal */
+#define KP_SUPERCMD() kp_get_supercmd()
+#define KP_SU_PATH()  kp_get_su_path()
+
 #define SUPERCMD (KP_SUPERCMD())
 #define SU_PATH  (KP_SU_PATH())
+
+#endif /* !__ASSEMBLER__ */
